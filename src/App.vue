118fd91e0facc74d3851b45dffecaba4d9880583
Remco Lakens <remco.lakens@gmail.com>
2023-10-07 15:43:51 +0200

    feat: update packages

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,26 @@
 </template>
 
 <script>
-import Fibonacci from './components/Fibonacci.vue'
+import Fibonacci from "./components/Fibonacci.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-      Fibonacci
-  }
-}
+    Fibonacci,
+  },
+};
 </script>
 
 <style lang="scss">
-    @import "~sanitize.css";
+html {
+  font-size: 0.625rem;
+}
 
-    html {
-        font-size: 0.625rem;
-    }
-
-    body {
-        font-size: 1.6rem;
-        line-height: 1.625;
-        font-family: Helvetica, Arial, sans-serif;
-        -webkit-text-size-adjust: 100%;
-        -webkit-font-smoothing: antialiased;
-    }
+body {
+  font-size: 1.6rem;
+  line-height: 1.625;
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+}
 </style>
